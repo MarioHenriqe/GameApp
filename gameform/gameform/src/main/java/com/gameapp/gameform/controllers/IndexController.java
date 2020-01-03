cf2b22845f9dd1fada47b1398ee5635f47667f9c
MarioHenriquedaSilva <mario.santos17@fatec.sp.gov.br>
2020-01-03 14:17:30 -0300

    Log

--- gameform/gameform/src/main/java/com/gameapp/gameform/controllers/IndexController.java
+++ gameform/gameform/src/main/java/com/gameapp/gameform/controllers/IndexController.java
@@ -1,14 +1,18 @@
 package com.gameapp.gameform.controllers;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
     public String Index(){
+
         return "index";
     }
 }
